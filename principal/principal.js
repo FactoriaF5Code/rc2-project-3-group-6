8e787012f69dd87500a78806f2068b5d8bc706e6
herminio castro riveira <hxssport@gmail.com>
2023-12-11 15:15:41 +0100

    implementada reproduccion de audios

--- principal/principal.js
+++ principal/principal.js
@@ -3,27 +3,49 @@ let personaje1 = document.querySelector(".funko1");
 let audio1 = document.querySelector(".audio1");
 
 boton1.onclick = function () {
-    personaje1.classList.toggle("bailar");
-    audio1.toggleAttribute("muted");
-}
+  personaje1.classList.toggle("bailar");
+  if (audio1.muted) {
+    audio1.muted = false;
+  } else {
+    audio1.muted = true;
+  }
+};
 
 let boton2 = document.querySelector(".boton2");
 let personaje2 = document.querySelector(".funko2");
+let audio2 = document.querySelector(".audio2");
 
 boton2.onclick = function () {
-    personaje2.classList.toggle("bailar");
-}
+  personaje2.classList.toggle("bailar");
+  if (audio2.muted) {
+    audio2.muted = false;
+  } else {
+    audio2.muted = true;
+  }
+};
 
 let boton3 = document.querySelector(".boton3");
 let personaje3 = document.querySelector(".funko3");
+let audio3 = document.querySelector(".audio3");
 
 boton3.onclick = function () {
-    personaje3.classList.toggle("bailar");
-}
+  personaje3.classList.toggle("bailar");
+  if (audio3.muted) {
+    audio3.muted = false;
+  } else {
+    audio3.muted = true;
+  }
+};
 
 let boton4 = document.querySelector(".boton4");
 let personaje4 = document.querySelector(".funko4");
+let audio4 = document.querySelector(".audio4");
 
 boton4.onclick = function () {
-    personaje4.classList.toggle("bailar");
-}
+  personaje4.classList.toggle("bailar");
+  if (audio4.muted) {
+    audio4.muted = false;
+  } else {
+    audio4.muted = true;
+  }
+};
