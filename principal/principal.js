cba5229dfe259d89c94694f69622fb9ffbb1b27a
Marta <martalarena2002@gmail.com>
2023-12-11 15:30:30 +0100

    los audios siguen sin estar sincronizados

--- principal/principal.js
+++ principal/principal.js
@@ -1,6 +1,26 @@
+let audio1 = document.querySelector(".audio1");
+let audio2 = document.querySelector(".audio2");
+let audio3 = document.querySelector(".audio3");
+let audio4 = document.querySelector(".audio4");
+
+document.addEventListener('mousemove', function(event) {
+    if (audio1.paused) {
+      audio1.play();
+    }
+    if (audio2.paused) {
+        audio2.play();
+      }
+    if (audio3.paused) {
+        audio3.play();
+      }
+    if (audio4.paused) {
+        audio4.play();
+      }
+  });
+
 let boton1 = document.querySelector(".boton1");
 let personaje1 = document.querySelector(".funko1");
-let audio1 = document.querySelector(".audio1");
+
 
 boton1.onclick = function () {
   personaje1.classList.toggle("bailar");
@@ -13,7 +33,7 @@ boton1.onclick = function () {
 
 let boton2 = document.querySelector(".boton2");
 let personaje2 = document.querySelector(".funko2");
-let audio2 = document.querySelector(".audio2");
+
 
 boton2.onclick = function () {
   personaje2.classList.toggle("bailar");
@@ -26,7 +46,7 @@ boton2.onclick = function () {
 
 let boton3 = document.querySelector(".boton3");
 let personaje3 = document.querySelector(".funko3");
-let audio3 = document.querySelector(".audio3");
+
 
 boton3.onclick = function () {
   personaje3.classList.toggle("bailar");
@@ -39,7 +59,7 @@ boton3.onclick = function () {
 
 let boton4 = document.querySelector(".boton4");
 let personaje4 = document.querySelector(".funko4");
-let audio4 = document.querySelector(".audio4");
+
 
 boton4.onclick = function () {
   personaje4.classList.toggle("bailar");
