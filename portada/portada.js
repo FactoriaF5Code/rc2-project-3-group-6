c21918b066118b16fa8aa5eeb5453d40a3ea41c1
herminiocastro <hxssport@gmail.com>
2023-12-15 10:55:28 +0100

    cambio ruta img portada

--- portada/portada.js
+++ portada/portada.js
@@ -1,7 +1,7 @@
 let tiempoEspera = 10000;
 
 function redirigir() {
-  window.location.href = "/principal/principal.html";
+  window.location.href = "../principal/principal.html";
 }
 
 setTimeout(redirigir, tiempoEspera);
